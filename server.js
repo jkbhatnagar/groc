b2fc8e280389b3f9acbee203cb0bd102f47e765a
jkbhatnagar <jatin@192-168-1-105.tpgi.com.au>
2016-01-26 00:08:38 +1100

    changed for OpenShift Hosting

--- server.js
+++ server.js
@@ -5,14 +5,26 @@ var bodyParser = require('body-parser');
 var routes     = require('./routes');
 
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/groc')
+var db_name = 'groc';
+
+//provide a sensible default for local development
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
+// mongoose.connect('mongodb://localhost/groc')
+mongoose.connect(mongodb_connection_string);
 
 // Express app will use body-parser to get data from POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Set port
-var port = process.env.PORT || 8080;        // set the port
+// var port = process.env.PORT || 8080;        // set the port
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 // Define a prefix for all routes
 // Can define something unique like MyRestAPI
@@ -20,5 +32,8 @@ var port = process.env.PORT || 8080;        // set the port
 app.use('/', routes);
 
 // Start server listening on port 8080
-app.listen(port);
-console.log('RESTAPI listening on port: ' + port);
+// app.listen(port);
+// console.log('RESTAPI listening on port: ' + port);
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
