cb54af45f438eddf965fbfed33d2827612126089
jkbhatnagar <jatin@192-168-1-104.tpgi.com.au>
2016-02-07 19:12:49 +1100

    missing comma

--- routes.js
+++ routes.js
@@ -139,7 +139,7 @@ router.route('/authenticate')
                 res.json({
                   status: 'success',
                   message: 'Enjoy your token!',
-                  validityInSeconds: 86400
+                  validityInSeconds: 86400,
                   token: token,
                   data: user
                 });
